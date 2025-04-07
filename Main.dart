7f8598040e03e43e7b07c2c5f93849e9e49f3d7d
Youssef Atia <128436665+YoussefAtia17@users.noreply.github.com>
2025-04-07 21:29:08 +0200

    Add-Stop

--- Main.dart
+++
@@ -1,8 +0,0 @@
-@override
-widget build(BuildContext context) {
-return MaterialApp(
-debugShowCheckedModeBanner: false,
-title: "Flutter Advanced",
-theme: ThemeData(),
-);
-}
---
+++ lib/Main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'doc_app.dart';
+import 'Core/routing/app_router.dart';
+
+void main() {
+  runApp(DocApp(
+    appRouter: AppRouter(),
+  )); // DocApp
+}
